d7befed84325e5b09337dd35413aaff4856ec59e
DINESH KUMAR <abhikumar45444@gmail.com>
2022-09-30 23:51:10 +0530

    Initial Commit Project Uploaded

---
+++ CurrencyConverter_static1/MainWindow.xaml.cs
@@ -0,0 +1,229 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+//This library is used for Regular Expression
+using System.Text.RegularExpressions;
+
+//This library is used for DataTable
+using System.Data;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System;
+using Newtonsoft.Json;
+
+namespace CurrencyConverter_static1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Root val = new Root();
+
+        public class Root    //Root Class is a main class. API returns rates in a rates it Returns all Currecny Name with Value.
+        { 
+            public Rate rates { get; set; } // get all record in rates and ser in rate clas as Currency name wise
+            public long timestamp;
+            public string license;
+
+
+        }
+
+        public class Rate  //make sure API returns NAmes and where you want to store that NAme are same. lke in API Get Response
+        { 
+            public double INR { get; set; }
+            public double JPY { get; set; }
+            public double USD { get; set; }
+            public double NZD { get; set; }
+            public double EUR { get; set; }
+            public double CAD { get; set; }
+            public double ISK { get; set; }
+            public double PHP { get; set; }
+            public double DKK { get; set; }
+            public double CZK { get; set; }
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //ClearControls method is used to clear all control values
+            ClearControls();
+
+            GetValue();
+            ////BindCurrency is used to bind currency name with the value in the Combobox
+            //BindCurrency();
+        }
+
+
+        private async void GetValue()
+        {
+            val = await GetData<Root>("https://openexchangerates.org/api/latest.json?app_id=f8941b5fc5a042f5b2e02ae97c01f09b");
+            BindCurrency();
+        }
+
+        public static async Task<Root> GetData<T>(string url)
+        {
+            var myRoot = new Root();
+            try
+            {
+                using (var client = new HttpClient())  //HttpClient class provides a base class for sending/recieving the HTTP request 
+                {
+                    client.Timeout = TimeSpan.FromMinutes(1);   //thw timespan to wait before the request times out.
+                    HttpResponseMessage response = await client.GetAsync(url);  //HttpResponseMessage is a way of returning a message
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)   //check API response status code ok
+                    {
+                        var ResponceString = await response.Content.ReadAsStringAsync();   //Sreialize the HTTP content to a string
+                        var ResponceObject = JsonConvert.DeserializeObject<Root>(ResponceString);   //JsonCOnvert.DeserializeObject
+
+                        //MessageBox.Show("Timestamp: " + ResponceObject.timestamp, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                        //MessageBox.Show("Rates: " + ResponceString, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        return ResponceObject; //return API response
+                    }
+                    return myRoot;
+                }
+            }
+            catch
+            {
+                return myRoot;
+            }
+           
+        }
+
+        private void BindCurrency()
+
+        {
+            //Create a Datatable Object
+            DataTable dt = new DataTable();
+
+            //Add the text column in the DataTable
+            dt.Columns.Add("Text");
+
+            //Add the value column in the DataTable
+            dt.Columns.Add("Value");
+
+            //Add rows in the Datatable with text and value
+            dt.Rows.Add("--SELECT--", 0);
+            dt.Rows.Add("INR", val.rates.INR);
+            dt.Rows.Add("USD", val.rates.USD);
+            dt.Rows.Add("NZD", val.rates.NZD);
+            dt.Rows.Add("JPY", val.rates.JPY);
+            dt.Rows.Add("EUR", val.rates.EUR);
+            dt.Rows.Add("CAD", val.rates.CAD);
+            dt.Rows.Add("ISK", val.rates.ISK);
+            dt.Rows.Add("PHP", val.rates.PHP);
+            dt.Rows.Add("DKK", val.rates.DKK);
+            dt.Rows.Add("CZK", val.rates.CZK);
+
+
+            //Datatable data assigned from the currency combobox
+            cmbFromCurrency.ItemsSource = dt.DefaultView;
+
+            //DisplayMemberPath property is used to display data in the combobox
+            cmbFromCurrency.DisplayMemberPath = "Text";
+
+            //SelectedValuePath property is used to set the value in the combobox
+            cmbFromCurrency.SelectedValuePath = "Value";
+
+            //SelectedIndex property is used to bind the combobox to its default selected item 
+            cmbFromCurrency.SelectedIndex = 0;
+
+            //All properties are set to To Currency combobox as it is in the From Currency combobox
+            cmbToCurrency.ItemsSource = dt.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
+        }
+
+
+        //Convert the button click event
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            //Create the variable as ConvertedValue with double datatype to store currency converted value
+            double ConvertedValue;
+
+            //Check if the amount textbox is Null or Blank
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                //If amount textbox is Null or Blank it will show this message box
+                MessageBox.Show("Please Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                //After clicking on messagebox OK set focus on amount textbox
+                txtCurrency.Focus();
+                return;
+            }
+            //Else if currency From is not selected or select default text --SELECT--
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                //Show the message
+                MessageBox.Show("Please Select Currency From", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //Set focus on the From Combobox
+                cmbFromCurrency.Focus();
+                return;
+            }
+            //Else if currency To is not selected or select default text --SELECT--
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                //Show the message
+                MessageBox.Show("Please Select Currency To", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //Set focus on the To Combobox
+                cmbToCurrency.Focus();
+                return;
+            }
+
+            //Check if From and To Combobox selected values are same
+            if (cmbFromCurrency.Text == cmbToCurrency.Text)
+            {
+                //Amount textbox value set in ConvertedValue.
+                //double.parse is used for converting the datatype String To Double.
+                //Textbox text have string and ConvertedValue is double Datatype
+                ConvertedValue = double.Parse(txtCurrency.Text);
+
+                //Show the label converted currency and converted currency name and ToString("N3") is used to place 000 after the dot(.)
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+            else
+            {
+                //Calculation for currency converter is From Currency value multiply(*) 
+                //With the amount textbox value and then that total divided(/) with To Currency value
+                ConvertedValue = (double.Parse(cmbToCurrency.SelectedValue.ToString()) * double.Parse(txtCurrency.Text)) / double.Parse(cmbFromCurrency.SelectedValue.ToString());
+
+                //Show the label converted currency and converted currency name.
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+        }
+
+
+        //Clear Button click event
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            //ClearControls method is used to clear all controls value
+            ClearControls();
+        }
+
+
+        //ClearControls method is used to clear all controls value
+        private void ClearControls()
+        {
+            txtCurrency.Text = string.Empty;
+            if (cmbFromCurrency.Items.Count > 0)
+                cmbFromCurrency.SelectedIndex = 0;
+            if (cmbToCurrency.Items.Count > 0)
+                cmbToCurrency.SelectedIndex = 0;
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e) //Allow Only Integer in Text Box
+        {
+            //Regular Expression is used to add regex.
+            // Add Library using System.Text.RegularExpressions;
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# Currency Converter Application
+ 
+Get accurate and up-to-the-minute currency conversion rates for pair of currencies.
+ 
+ ![thumbnail](./AppImage.png)
+
+## DESCRIPTION
+ 1. A Currency Converter is a WPF application designed to convert one currency into another to check its corresponding value.
+ 2. It uses WPF which stands for Windows Presentation Foundation. And, the WPF is a UI framework that creates desktop client applications. Also, it is a part of .NET.
+ 3. It uses API request to world wide web through which current exchange rates are evaluated.
+
+## INSTALLATION
+```console
+Run setup.exe to install the application.
+```
+
+## BUILD: If you want to make changes feel free to do so
+```console 
+Open CurrencyConverter_static1.sln file in Microsoft Visual Studio
+Click on start
+```
+## TECHNOLOGIES
+|Technologies|
+|------|
+|C#|
+|WPF (Windows Presentation Form)|
+|XAML|
+|DotNet|
+|API - https://exchangeratesapi.io|
+
+
+ 
+ 
+ 
+ 
+ 
